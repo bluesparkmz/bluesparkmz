6a2687d9a8656be88524f59aa7bad58e3fb07624
Bluespark MZ <bluesparkmz1@gmail.com>
2026-01-13 16:35:02 +0200

    ol

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,24 +1,37 @@
-import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
+"use client";
+
+import Link, { LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
-interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
-  className?: string;
+interface NavLinkCompatProps extends Omit<LinkProps, "className"> {
+  to: string;
+  className?: string | ((props: { isActive: boolean }) => string);
   activeClassName?: string;
-  pendingClassName?: string;
+  children: React.ReactNode;
 }
 
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
+  ({ className, activeClassName, to, children, ...props }, ref) => {
+    const pathname = usePathname();
+    // Simple exact match for active state. 
+    // You might want 'startsWith' depending on requirement, but exact is safer for now.
+    const isActive = pathname === to;
+
+    const computedClassName = typeof className === "function"
+      ? className({ isActive })
+      : cn(className as string, isActive && activeClassName);
+
     return (
-      <RouterNavLink
+      <Link
         ref={ref}
-        to={to}
-        className={({ isActive, isPending }) =>
-          cn(className, isActive && activeClassName, isPending && pendingClassName)
-        }
+        href={to}
+        className={computedClassName}
         {...props}
-      />
+      >
+        {children}
+      </Link>
     );
   },
 );
